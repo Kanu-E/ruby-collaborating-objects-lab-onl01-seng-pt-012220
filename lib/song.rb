0dbd0c2627399b5221c7f3f4a7519203b8d75155
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-21 21:05:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,9 @@ class Song
  
  def self.new_by_filename(file_name)
   art = file_name.split(" - ")[0]
-song = self.new(file_name.split(" - ")[1])
+  song = self.new(file_name.split(" - ")[1])
    song.artist = Artist.find_or_create_by_name(art)
+   song
  end
  
 end
