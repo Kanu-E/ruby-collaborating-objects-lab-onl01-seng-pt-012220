8d32f6de3154cac8016b834874a007c8094c0a41
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-21 21:10:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,11 +10,11 @@ class Song
   @@all
  end
  
- def self.new_by_filename(file_name)
+def self.new_by_filename(file_name)
   art = file_name.split(" - ")[0]
   song = self.new(file_name.split(" - ")[1])
    song.artist = Artist.find_or_create_by_name(art)
-   song
+  song
  end
  
 end
