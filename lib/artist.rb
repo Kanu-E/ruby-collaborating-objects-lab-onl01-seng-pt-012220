cd1eee5dac59cb922e4565075e88950377cab568
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-21 21:45:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
    find_or_create_name = @@all.find do |artist| artist.name == name 
    end  
    if find_or_create_name == nil
-     artist self.new(name)
+      self.new(name)
      @@all << artist
    else
     find_or_create_name
